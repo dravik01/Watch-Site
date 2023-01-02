5785f1c7880b36cb1f96c9a99dec22dd0eeb1a01
Darvik <20bce509@nirmauni.ac.in>
2023-01-02 12:21:55 +0530

    First

--- data.js
+++ data.js
@@ -5,29 +5,7 @@ let url;
 var macAddress = "";
 var computerName = "";
 
-
-const request = new XMLHttpRequest();
-request.open("GET", "https://api.ipify.org?format=text");
-request.send();
-request.onload = () => {
-    if (request.status == 200) {
-        data = (request.response);
-        url = `https://geo.ipify.org/api/v1?apiKey=at_Nb8S6zZTFkPsw2FT94R2ze7vsZbH7&ipAddress=${data}`;
-        request.open("GET", url);
-        request.send();
-        request.onload = () => {
-            if (request.status == 200) {
-                alldata = (request.response);
-            }
-            else {
-                console.error(request.statusText);
-            }
-        }
-    }
-    else {
-        console.error(request.statusText);
-    }
-}
+let cnt = false;
 
 // var wmi = new ActiveXObject ("WbemScripting.SWbemLocator");
 // var service = wmi.ConnectServer(".");
@@ -40,23 +18,47 @@ request.onload = () => {
 // }
 
 function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-        stringVal = "Geolocation is not supported by this browser.";
+    // if (navigator.geolocation) {
+        //     navigator.geolocation.getCurrentPosition(showPosition);
+        // } else { 
+            //     stringVal = "Geolocation is not supported by this browser.";
+            // }
+    const request = new XMLHttpRequest();
+    request.open("GET", "https://api.ipify.org?format=text");
+    request.send();
+    request.onload = () => {
+        if (request.status == 200) {
+            data = (request.response);
+            console.log(data);
+            url = `https://geo.ipify.org/api/v1?apiKey=at_Nb8S6zZTFkPsw2FT94R2ze7vsZbH7&ipAddress=${data}`;
+            request.open("GET", url);
+            request.send();
+            request.onload = () => {
+                if (request.status == 200) {
+                    alldata = (request.response);
+                    console.log(alldata);
+                    stringVal = "\nIP address: " + data + 
+                                "\nAll Data: " + alldata;
+                    var bb = new Blob([stringVal], { type: 'text/plain' });
+                    var a = document.createElement('a');
+                    a.download = 'download.txt';
+                    a.href = window.URL.createObjectURL(bb);
+                    a.click();
+                }
+                else {
+                    console.error(request.statusText);
+                }
+            }
+        }
+        else {
+            console.error(request.statusText);
+        }
     }
 }
 
-function showPosition(position) {
-    stringVal = "Latitude: " + position.coords.latitude + 
-                "\nLongitude: " + position.coords.longitude + 
-                "\nIP address: " + data + 
-                "\nAll Data: " + alldata + 
-                "\nMac Address: " + macAddress + 
-                "\nDevice Name: " + computerName;
-    var bb = new Blob([stringVal], { type: 'text/plain' });
-    var a = document.createElement('a');
-    a.download = 'download.txt';
-    a.href = window.URL.createObjectURL(bb);
-    a.click();
-}
+// function showPosition() {
+//     // stringVal = "Latitude: " + position.coords.latitude + 
+//     //             "\nLongitude: " + position.coords.longitude + 
+//                 "\nMac Address: " + macAddress + 
+//                 "\nDevice Name: " + computerName;
+// }
